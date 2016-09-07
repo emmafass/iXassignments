dac19400da25eafe87761cacf37d07e39a4eea84
Emma <enf5cb@virginia.edu>
2016-09-07 00:11:26 -0400

    Updated content

--- Day13/script.js
+++ Day13/script.js
@@ -2,13 +2,13 @@ var app = angular.module('tensionApp',['firebase','ngRoute']);
 
 
 app.run(["$rootScope", "$location", function($rootScope, $location) {
-  $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+	$rootScope.$on("$routeChangeError", function(event, next, previous, error) {
     // We can catch the error thrown when the $requireSignIn promise is rejected
     // and redirect the user back to the home page
     if (error === "AUTH_REQUIRED") {
-      $location.path("/signup");
+    	$location.path("/signup");
     }
-  });
+});
 }]);
 
 
@@ -22,8 +22,8 @@ app.config(function($routeProvider){
 			//returns a promise... you can use .then ... the code takes some time to execute
 			//the promise that it gives is Requires Sign In... firebase must provide authentication
 			"currentAuth": function($firebaseAuth) {
-	        return $firebaseAuth().$requireSignIn();
-	      }
+				return $firebaseAuth().$requireSignIn();
+			}
 		}
 	})
 	$routeProvider.when('/channel/:channelId', {
@@ -31,8 +31,8 @@ app.config(function($routeProvider){
 		templateUrl: "/templates/home.html",
 		resolve: {
 			"currentAuth": function($firebaseAuth) { //Current auth can be used in the associated controller
-	        return $firebaseAuth().$requireSignIn();
-	      }
+				return $firebaseAuth().$requireSignIn();
+			}
 		}
 	})
 	$routeProvider.when('/signup', {
@@ -48,11 +48,49 @@ app.config(function($routeProvider){
 
 
 
-app.controller("listCtrl", function($scope, $firebaseObject, $firebaseArray, $routeParams, $firebaseAuth) {
+app.controller("listCtrl", function($http, $scope, $firebaseObject, $firebaseArray, $routeParams, $firebaseAuth) {
 	var ref = firebase.database().ref().child('channels');
 	$scope.channels = $firebaseObject(ref);
 	$scope.channelId = $routeParams.channelId;
 	$scope.authObj = $firebaseAuth();
+
+
+	// var submitButton = document.getElementById("submit_form");
+	// var form = document.getElementById("email_form");
+	// form.addEventListener("submit", function (e) {
+	// 	setTimeout(function() {
+	// 		submitButton.value = "Sending...";
+	// 		submitButton.disabled = true;
+	// 		console.log("sent email");
+	// 	}, 1);
+	// });
+
+
+	var userRef = firebase.database().ref().child('users');
+	$scope.users = $firebaseArray(userRef);
+	// console.log("this is the user object");
+	// console.log(users);
+
+	$scope.sendMail = function() {
+		var email = $scope.recipient;
+
+		window.location.href = ("mailto:" + email +'?subject=hello&body=the_body&attachment=pdf.pdf');
+    	// window.open('mailto:'+email+'?subject=hello&body=the_body');
+    	$scope.recipient = "";
+    };
+
+
+
+    $scope.role = "my Current role";
+
+    $scope.clicked = function() {
+    	$scope.role = "I was clicked";
+    }
+
+
+
+
+
 	// $scope.curr_Auth = currentAuth;
 	// console.log($scope.channels);
 	// $scope.channel.name = "General";
@@ -73,6 +111,7 @@ app.controller("listCtrl", function($scope, $firebaseObject, $firebaseArray, $ro
 	// 	$location.path('/login');
 	// 	console.log("signed out");
 	// };
+
 
 });
 
@@ -148,7 +187,7 @@ app.controller("loginCtrl", function($scope, $firebaseObject, $firebaseArray, $f
 			console.error("Authentication failed:", error);
 			$scope.error_message = error.message;
 		});
-			
+
 	}
 
 });
@@ -159,6 +198,7 @@ app.controller("loginCtrl", function($scope, $firebaseObject, $firebaseArray, $f
 
 
 
+// <a href="mailto:"+ email + "?subject=my subject&body=see attachment&attachment="+"/my_location_virtual_path/myfile.lis">hi</a>
 
 
 
--- Day8/grocery.js
+++ Day8/grocery.js
@@ -6,15 +6,28 @@ app.controller('groceryCtrl', function($scope) {
 	$scope.addItem = function() {
 		var isIncluded = false;
 		for(var i = 0 ; i < $scope.groceries.length; i++) {
-			if ($scope.groceries[i].name === $scope.item) {
+			if ($scope.groceries[i].name === $scope.item.toLowerCase()) {
 				$scope.groceries[i].amt += parseInt($scope.amount);
 				isIncluded = true;
 			}
 		}
 		if (isIncluded === false){
+			if (isNaN($scope.item) === false){
+				console.log("Added a number");
+				$scope.item = "";
+				$scope.amount = "";
+				return;
+			}
+			if (isNaN($scope.amount) === true){
+				console.log("Added a number");
+				$scope.item = "";
+				$scope.amount = "";
+				return;
+			}
 			var newItem = {
-				name: $scope.item,
-				amt: parseInt($scope.amount)
+				name: $scope.item.toLowerCase(),
+				amt: parseInt($scope.amount),
+				"isEditing": false
 			};
 			$scope.groceries.push(newItem);
 		}
@@ -30,6 +43,19 @@ app.controller('groceryCtrl', function($scope) {
 		if(item.amt != 0) {
 			item.amt -= 1;
 		}
+		else {
+			$scope.item.delete();
+		}
+	};
+
+	$scope.delete = function(i) {
+		$scope.groceries.splice(i,1);
+	};
+
+	$scope.empty = function() {
+		// for(var i = 0; i <$scope.groceries.length; i++) {
+			$scope.groceries = [];
+		// }
 	};
 
 });
